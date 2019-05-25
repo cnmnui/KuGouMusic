b58eec699f3516f7578691baa4c7921288ee30ec
cnmnui <50909979+cnmnui@users.noreply.github.com>
2019-05-25 16:09:27 +0800

    Update KuGou.py

--- KuGou.py
+++ KuGou.py
@@ -19,29 +19,33 @@ class KugouSpider(scrapy.Spider):
     item = KugoumusicItem()
     k_num = math.floor(1e16 * random.random())
     search_base_url = "https://songsearch.kugou.com/song_search_v2?"
-    k_keyword = '周华健'
-    k_pagesize = '5'
-    k_time = round(time.time() * 1000)
+    k_keyword = '周华健'                              # 搜索关键词
+    k_pagesize = '5'                                 # 每页加载数量
+    k_time = round(time.time() * 1000)               # 时间值
 
     # 构造起始url
     k_data = {
         'keyword': k_keyword,                          # (搜索内容)
-        'page': '1',                                 # (起始页)
+        'page': '1',                                   # (起始页)
         'pagesize': k_pagesize,                        # (每页数量)
-        'userid': '-1',
+        'userid': '-1',                                # 用户ID, 未登录为-1
         'clientver': '',
         'platform': 'WebFilter',
         'tag': 'em',
         'filter': '2',
         'iscorrection': '1',
         'privilege_filter': '0',
-        '_': k_time
+        '_': k_time                                     # 时间值
     }
     k_params = urlencode(k_data)
     search_url = search_base_url + k_params
     start_urls = [search_url]
 
     def parse(self, response):
+        """
+        对起始页解析
+        得到并返回歌曲信息
+        """
         response.body.decode('utf-8')
         base_list = json.loads(response.text)['data']['lists']
         for base_info in base_list:
@@ -49,6 +53,7 @@ class KugouSpider(scrapy.Spider):
             file_hash = base_info['FileHash']
             k_Privilege = base_info['Privilege']
             song_base_url = 'https://wwwapi.kugou.com/yy/index.php?'
+            # 构造请求链接
             data = {
                 'r': 'play/getdata',
                 'callback': 'jQuery1910%s_%s'.format(self.k_num, self.k_time),
@@ -64,6 +69,10 @@ class KugouSpider(scrapy.Spider):
                 yield scrapy.Request(song_url, callback=self.parse_song_list)
 
     def parse_song_list(self, response):
+        """
+        对歌曲信息页进行解析
+        获取到所要抓取的歌曲信息
+        """
         response.body.decode('utf-8')
         song_info = json.loads(response.text[16: -2])
         self.item['song_name'] = song_info['data']['audio_name']
@@ -78,6 +87,9 @@ class KugouSpider(scrapy.Spider):
         yield self.item
 
     def parse_song(self, song_link):
+        """
+        解析歌曲链接
+        """
         ua = UserAgent()
         headers = {"UserAgent": ua.random}
         k_res = requests.get(song_link, headers=headers)
