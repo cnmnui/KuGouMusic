2fc46810843b6d30ffa7ef52b390a86f16fbbce8
cnmnui <50909979+cnmnui@users.noreply.github.com>
2019-05-27 11:54:36 +0800

    Update pipelines.py

--- pipelines.py
+++ pipelines.py
@@ -4,23 +4,22 @@ import pymysql
 from twisted.enterprise import adbapi
 import sys
 sys.path.append('你的路径')
-from KuGouMusic import settings
+m KuGouMusic.settings import Mysql_Info
 
 
+# 保存歌曲文件
 class KuGouMusicPipeline(object):
-    # 保存歌曲到本地文件
     def process_item(self, item, spider):
         with open(item['song_path'], 'wb') as f:
                 f.write(item['song'])
         self.to_mysql(item)
 
-    # 保存歌曲信息到数据库
     def to_mysql(self, item):
-        m_host = settings.MYSQL_HOST
-        m_user = settings.MYSQL_USER
-        m_psword = settings.MYSQL_PASSWORD
-        m_db = settings.MYSQL_DBNAME
-        m_table = settings.MYSQL_TABLE
+        m_host = Mysql_Info['host']
+        m_user = Mysql_Info['user']
+        m_psword = Mysql_Info['password']
+        m_db = Mysql_Info['database']
+        m_table = Mysql_Info['table']
         conn = pymysql.connect(m_host, m_user, m_psword, m_db, charset='utf8mb4')
         cursor = conn.cursor()
         try:
@@ -28,6 +27,7 @@ class KuGouMusicPipeline(object):
                   "values(%s,%s,%s,%s)" .format(m_db, m_table)
             m_values = (item['audio_id'], item['song_name'], item['author_name'], item['song_path'])
             cursor.execute(ins, m_values)
+
         except ValueError as e:
             print(e)
             conn.rollback()
@@ -35,7 +35,6 @@ class KuGouMusicPipeline(object):
             conn.commit()
             cursor.close()
             conn.close()
-            
             
             
             
